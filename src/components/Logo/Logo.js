b274bdf4c3b50d271f70ff57d45a50de263a606d
s3539822 <41284457+s3539822@users.noreply.github.com>
2021-01-09 16:51:46 +1100

    Logo

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -4,7 +4,7 @@ import classNames from 'classnames';
 
 import config from '../../config';
 import IconLogo from './IconLogo';
-import LogoImage from './saunatime-logo.png';
+import LogoImage from './campwhere-logo.png';
 import css from './Logo.module.css';
 
 const Logo = props => {
