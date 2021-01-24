a09bd13b5b48882ab767cc4d5d8a378d11a42fce
s3539822 <s3539822@student.rmit.edu.au>
2021-01-24 21:07:43 +1100

    Contact Page Template

---
+++ src/containers/ContactPage/ContactPage.js
@@ -0,0 +1,93 @@
+import React from 'react';
+import config from '../../config';
+import { twitterPageURL } from '../../util/urlHelpers';
+import { StaticPage, TopbarContainer } from '../../containers';
+import {
+  LayoutSingleColumn,
+  LayoutWrapperTopbar,
+  LayoutWrapperMain,
+  LayoutWrapperFooter,
+  Footer,
+  ExternalLink,
+} from '../../components';
+
+import css from './ContactPage.module.css';
+import image from '../../assets/background-1440.jpg';
+import { FormattedMessage } from '../../util/reactIntl';
+
+const ContactPage = () => {
+  const { siteTwitterHandle, siteFacebookPage } = config;
+  const siteTwitterPage = twitterPageURL(siteTwitterHandle);
+
+  // prettier-ignore
+  return (
+    <StaticPage
+      title="Contact Us"
+      schema={{
+        '@context': 'http://schema.org',
+        '@type': 'ContactPage',
+        description: 'Contact CampWhere',
+        name: 'Contact page',
+      }}
+    >
+      <LayoutSingleColumn>
+        <LayoutWrapperTopbar>
+          <TopbarContainer />
+        </LayoutWrapperTopbar>
+
+        <LayoutWrapperMain className={css.staticPageWrapper}>
+          {/*<h1 className={css.pageTitle}>
+            <FormattedMessage id="AboutPage.pageTitle" />
+          </h1>
+          <img className={css.coverImage} src={image} alt="CampWhere background." />
+
+          <div className={css.contentWrapper}>
+            <div className={css.contentSide}>
+              <p>
+                <FormattedMessage id="AboutPage.sideFact" />
+              </p>
+            </div>
+
+            <div className={css.contentMain}>
+              <h2>
+                <FormattedMessage id="AboutPage.paraMainTitle" />
+              </h2>
+
+              <p>
+                <FormattedMessage id="AboutPage.body1Text" />
+              </p>
+
+              <h3 className={css.subtitle}>
+                <FormattedMessage id="AboutPage.body2Header" />
+              </h3>
+
+              <p>
+                <FormattedMessage id="AboutPage.body2Text" />
+              </p>
+
+              <p>
+                <FormattedMessage id="AboutPage.callToAction1" />
+
+                <ExternalLink href={siteFacebookPage}>
+                  <FormattedMessage id="AboutPage.callToAction2" />
+                </ExternalLink>
+
+                <FormattedMessage id="AboutPage.callToAction3" />
+
+                <ExternalLink href={siteTwitterPage}>
+                  <FormattedMessage id="AboutPage.callToAction4" />
+                </ExternalLink>.
+              </p>
+            </div>
+          </div>*/}
+        </LayoutWrapperMain>
+
+        <LayoutWrapperFooter>
+          <Footer />
+        </LayoutWrapperFooter>
+      </LayoutSingleColumn>
+    </StaticPage>
+  );
+};
+
+export default ContactPage;
